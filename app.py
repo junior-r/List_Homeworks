97f6b7be9f3582208c4712e8e753f75ba38a6664
Junior Ruiz <junior31064049@gmail.com>
2022-02-18 15:34:09 -0400

    Se agregó la función que pide los datos necesarios para crear una tarea

--- app.py
+++ app.py
@@ -17,18 +17,41 @@ except sqlite3.OperationalError:
   pass
 
 materias = {
-  'Física': '1',
-  'Matemáticas': '2',
-  'Química': '3',
-  'Biología': '4',
-  'Educación Física': '5',
-  'Castellano': '6',
-  'F.S.N.': '7',
-  'Inglés': '8',
-  'G.H.C.': '9',
-  'Ciencias de la Tierra': '10',
-  'Grupo (Electiva)': '11',
+  '1':'Física',
+  '2':'Matemáticas',
+  '3':'Química',
+  '4':'Biología',
+  '5':'Educación Física',
+  '6':'Castellano',
+  '7':'F.S.N.',
+  '8':'Inglés',
+  '9':'G.H.C.',
+  '10':'Ciencias de la Tierra',
+  '11':'Grupo (Electiva)',
 }
+
+
+def dates_for_task(materias):
+  print('---------- Creating Tasks ----------')
+  for k, v in materias.items():
+    if len(k) != 1:
+      print(k, '-', v)
+    else:
+      print(k, '-' * 2, v)
+  materia = input('Ingrese el número de la Materia: ')
+  tipo_tarea = input('Ingrese el tipo de Tarea: ')
+  fecha_entrega = input('Siguiendo el formato DD/MM/AAAA. Ingrese la fecha de entrega: ')
+  for k, v in materias.items():
+    list_k = list(k)
+    k = ''.join(list_k)
+    if materia == k:
+      print('materia valida', v)
+
+
+crear_task = input('¿Desea crear una tarea?: ')
+while crear_task.lower() == 'si':
+  print(dates_for_task(materias))
+  crear_task = input('¿Desea crear una tarea?: ')
 
 conexion.commit()
 conexion.close()
