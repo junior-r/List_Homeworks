71d72bbaf33ab1de3064eb64de47f04bfc393d7f
Junior Ruiz <junior31064049@gmail.com>
2022-02-18 14:11:19 -0400

    Se agregó las materias

---
+++ app.py
@@ -0,0 +1,14 @@
+materias = {
+  'Física': '1',
+  'Matemáticas': '2',
+  'Química': '3',
+  'Biología': '4',
+  'Educación Física': '5',
+  'Castellano': '6',
+  'F.S.N.': '7',
+  'Inglés': '8',
+  'G.H.C.': '9',
+  'Ciencias de la Tierra': '10',
+  'Grupo (Electiva)': '11',
+}
+
